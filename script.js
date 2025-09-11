5529529f3ded9fb6b1a37f97bc8d6d8f6464be99
Jayson M. Dialde <jaysondialde13@gmail.com>
2025-09-11 22:14:48 +0800

    3rd commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+const themeToggle = document.getElementById("theme-icon");
+const body = document.body;
+
+// check saved theme
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  themeToggle.classList.replace("bx-moon", "bx-sun");
+}
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    themeToggle.classList.replace("bx-moon", "bx-sun");
+    localStorage.setItem("theme", "dark");
+  } else {
+    themeToggle.classList.replace("bx-sun", "bx-moon");
+    localStorage.setItem("theme", "light");
+  }
+});
